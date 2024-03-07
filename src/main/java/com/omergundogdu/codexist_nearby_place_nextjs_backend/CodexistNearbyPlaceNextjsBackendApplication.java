5adfc3ef3bc59b83c83b40876e103071dd471e50
ÖMER GÜNDOĞDU <omergundogdu75@gmail.com>
2024-03-07 20:37:43 +0300

    Initial commit

---
+++ src/main/java/com/omergundogdu/codexist_nearby_place_nextjs_backend/CodexistNearbyPlaceNextjsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.omergundogdu.codexist_nearby_place_nextjs_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodexistNearbyPlaceNextjsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodexistNearbyPlaceNextjsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/omergundogdu/codexist_nearby_place_nextjs_backend/CodexistNearbyPlaceNextjsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omergundogdu.codexist_nearby_place_nextjs_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodexistNearbyPlaceNextjsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
